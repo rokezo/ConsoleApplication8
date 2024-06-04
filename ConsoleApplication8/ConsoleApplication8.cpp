6a2c10774f75051927602c84ad603dbcf38024c0
rokezo <dobusnikita@gmail.com>
2024-06-04 21:14:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int arr[10]; 
+    arr[0] = n;
+
+    for (int i = 1; i < 10; i++) {
+        arr[i] = arr[i - 1] * 2;
+    }
+
+    cout << "Created array: ";
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
